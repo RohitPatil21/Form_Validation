dcea72354cb32aae1a032380afc060b729ab3597
Rohit <rohitpatil7@yahoo.com>
2022-01-09 13:10:27 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let passRef = document.getElementById(".password");
+let passValue = passRef.value;
+let confirmRef = document.getElementById(".confirm");
+let confirmValue = confirmRef.value;
+
+
+
+
+
+
